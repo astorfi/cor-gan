1ee6516f00d365c4721845e86fe621fd5b0c1c7a
Amirsina Torfi <amirsina.torfi@gmail.com>
2020-04-26 12:39:35 -0400

    Update vaeConvolutional.py

--- Generative/VAE/MIMIC/vaeConvolutional.py
+++ Generative/VAE/MIMIC/vaeConvolutional.py
@@ -4,8 +4,6 @@ import torch
 import torch.utils.data
 from torch import nn, optim
 from torch.nn import functional as F
-from torchvision import datasets, transforms
-from torchvision.utils import save_image
 import os
 import numpy as np
 from torch.utils.data import DataLoader
